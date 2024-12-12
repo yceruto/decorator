3d035c7a5e8d7ac294e368b7e2b4378e6ea489a1
Yonel Ceruto <yonel.ceruto@scnd.com>
2024-12-12 18:12:59 -0500

    Simplify decorated by itself case

--- tests/Fixtures/Decorator/Json.php
+++ tests/Fixtures/Decorator/Json.php
@@ -27,9 +27,4 @@ final class Json extends DecoratorAttribute implements DecoratorInterface
             return json_encode($result, JSON_THROW_ON_ERROR);
         };
     }
-
-    public function decoratedBy(): string
-    {
-        return self::class;
-    }
 }
